62bded0a08b043813fa1539679b658fcea2f18ee
Iggy <iggybiegler@gmail.com>
2015-03-01 05:26:49 +0100

    First submits

---
+++ foundation/elements/footer.php
@@ -0,0 +1,14 @@
+<?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
+
+<div class="responsive-footer row full-width">
+
+</div>
+@t(jquery)
+@x(Foundation_JS)
+<!-- add your own scripts.js when needed -->
+<!-- <script type="text/javascript" src="@t(themeURL)/js/scripts.js"></script> -->
+<script>
+	$(document).foundation();
+</script>
+</body>
+</html>
---
+++ foundation/elements/header.php
@@ -0,0 +1,14 @@
+<?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	@t(metaTitle { title: @p(meta_title) })
+	@x(seo)
+	<!-- add your own style.css here when needed -->
+	<!-- <link type="text/css" rel="stylesheet" href="@t(themeURL)/css/style.css" /> -->
+	@x(Foundation_CSS)
+</head>
+<body>
