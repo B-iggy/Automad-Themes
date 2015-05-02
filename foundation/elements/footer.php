863edc1ff3b6acb64759570047efa6797b379e37
B-iggy <iggybiegler@gmail.com>
2015-05-03 00:47:23 +0200

    Edit some lines. Cleaned up my website.

---
+++ foundation/elements/footer.php
@@ -0,0 +1,11 @@
+<?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
+
+<div class="responsive-footer row full-width">
+
+</div>
+@x(Foundation_JS)
+<!-- add your own scripts.js when needed -->
+<!-- <script type="text/javascript" src="@t(themeURL)/js/scripts.js"></script> -->
+@s(google_analytics_tracking_code)
+</body>
+</html>
